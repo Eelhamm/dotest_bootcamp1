b8151e666a7a365c371841cf22e6722e214e1525
kam.masoumi <kam.masoumi@gmail.com>
2018-08-27 18:10:33 +0430

    prime class

--- kamran.py
+++ kamran.py
@@ -1,5 +1,11 @@
-from random import randrange
 
+class prime:
+    def integer(self, number):
+        flag = 0
+        for i in range(2, number//2+1):
+            if number % 2 == 0:
+                flag = 1
+                break
+        return flag
 
-for i in range(1,12):
-    print(randrange(i))
+Prime = prime()
